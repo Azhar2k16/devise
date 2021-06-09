ecfe342ffacdb48b45f3ec95f44a9ab70e98c3de
Azhar2k16 <azharkalam05@gmail.com>
2021-06-09 15:31:59 +0500

    commit2.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
-User.create(email: 'user1@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
-User.create(email: 'user2@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
-User.create(email: 'user3@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
-User.create(email: 'user4@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
-User.create(email: 'user5@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
-User.create(email: 'user6@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
-User.create(email: 'user7@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
+User.create(email: 'admin@gmail.com', password: 'qweasd123', password_confirmation: 'qweasd123')
 User.update_all confirmed_at: DateTime.now
