4ebe86777eee3ee3db8f79f0a39eaa1898be3e00
Azhar2k16 <azharkalam05@gmail.com>
2022-03-05 04:03:55 +0500

    FormStyling

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   end
   root 'static_pages#landing_page'
   get "privacy", to: 'static_pages#privacy_policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
