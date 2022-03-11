34200eaaf36656582a1fc221565188b3bd9cc35a
Azhar2k16 <azharkalam05@gmail.com>
2022-03-11 07:19:27 +0500

    allow_confirmable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  # config.action_mailer.default_url_options = { :host => "o-authy.herokuapp.com" }
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
