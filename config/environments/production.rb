d8dbd2634bd044cb68fce17a4d2901d766a52f24
Azhar2k16 <azharkalam05@gmail.com>
2022-03-11 08:37:31 +0500

    config'd smpt sv

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,19 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_options = { :host => "o-authy.herokuapp.com", :protocol => "https" }
+  config.action_mailer.default_url_options = {host: "o-authy.herokuapp.com", protocol: "https"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              Rails.application.credentials.dig(:gmail, :smtp),
+    port:                 587,
+    domain:               'o-authy.heroku.com',
+    user_name:            Rails.application.credentials.dig(:gmail, :email),
+    password:             Rails.application.credentials.dig(:gmail, :pw),
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 
 
   # Eager load code on boot. This eager loads most of Rails and
