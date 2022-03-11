0a047a2baad14defc5b2de10f4a6fc2605a9549b
Azhar2k16 <azharkalam05@gmail.com>
2022-03-11 07:49:10 +0500

    roleble changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable, :lockable,
          :omniauthable, omniauth_providers: [:google_oauth2, :github, :twitter, :facebook]
-      include Roleable
-
-      def self.from_omniauth(access_token)
-      data = access_token.info
-      user = User.where(email: data['email']).first
-      unless user
-        user = User.create(
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
-      end
-      user.provider = access_token.provider
-      user.uid = access_token.uid
-      user.name = access_token.info.name
-      user.image = access_token.info.image
-      user.save
-      User.update_all confirmed_at: DateTime.now
-      user
+      
+  include Roleable
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.save
+    User.update_all confirmed_at: DateTime.now
+    user
   end
 
 
