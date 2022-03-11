0327cefd4746b64279a2dc1edd9f5dff43f9c4ac
Azhar2k16 <azharkalam05@gmail.com>
2022-03-11 07:54:37 +0500

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  # config.action_mailer.default_url_options = { :host => "o-authy.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "o-authy.herokuapp.com", :protocol => "https" }
 
 
   # Eager load code on boot. This eager loads most of Rails and
